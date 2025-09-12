1b3eb835dab57222b92ee9af0a715f0171fd73fc
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-09-13 00:20:16 +0600

    updated signup page

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -78,7 +78,7 @@ const SignUp = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#FCE4EC] to-[#E8EAF6] px-4 max-w-7xl py-16 mx-auto rounded-2xl">
+    <div className="min-h-screen flex my-16 items-center justify-center bg-gradient-to-br from-[#FCE4EC] to-[#E8EAF6] px-4 max-w-7xl py-16 mx-auto rounded-2xl">
       <ReTitle title="Medicare | SignUp"></ReTitle>
       <div className="bg-white rounded-2xl shadow-xl grid md:grid-cols-2 gap-10 p-8 max-w-5xl w-full">
         {/* Left Side: Lottie */}
